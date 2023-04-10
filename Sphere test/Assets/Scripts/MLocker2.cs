1d6a5e42cf7f593ec98dd1159c3d5bbea6a2bd54
Diegowned <dgt156423@gmail.com>
2023-04-10 10:17:20 -0400

    Confined mouse, more level design,etc

--- Sphere test/Assets/Scripts/MLocker2.cs
+++ Sphere test/Assets/Scripts/MLocker2.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class MLocker2 : MonoBehaviour
 {
     public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
 
     void Update()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
         SetCursorTexture();
     }
 
     private void SetCursorTexture()
     {
-        
+        Cursor.SetCursor(cursorTexture,hotSpot,cursorMode);
     }
 }
