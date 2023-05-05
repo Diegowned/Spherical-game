4eb9b113f94b246ed1c9e46c8ea8bf20e6bffdd5
Diegowned <dgt156423@gmail.com>
2023-05-05 10:59:22 -0400

    Main Menu UI Integrated

--- Sphere test/Assets/Scripts/MainMenu.cs
+++ Sphere test/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,10 @@ using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    private GameObject playerRock;
+    Animator rockAnimator;
+    private GameObject uiCanvas;
+    Animator canvasAnimator;
     bool fadeIsOn;
     int alpha = 255;
     [SerializeField] Image[] img = new Image[4];
@@ -13,7 +17,10 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRock = GameObject.Find("RocaPersonaje");
+        rockAnimator = playerRock.GetComponent<Animator>();
+        uiCanvas = GameObject.Find("Button Panel");
+        canvasAnimator = uiCanvas.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +34,19 @@ public class MainMenu : MonoBehaviour
             img[i].color = new Color32(255, 255, 255, (byte)alpha);
         for (int i = 0; i < 2; i++)
             txt[i].color = new Color32(255, 255, 255, (byte)alpha);
+
+        if (alpha < 1)
+            rockAnimator.SetBool("fadeComplete", true);
+        if (alpha < 1)
+            canvasAnimator.SetBool("buttonAppears", true);
+
+
+    }
+
+    IEnumerator ButtonAppears()
+    {
+        yield return new WaitForSeconds(2);
+        canvasAnimator.SetBool("buttonAppears", true);
     }
 
     private void FixedUpdate()
