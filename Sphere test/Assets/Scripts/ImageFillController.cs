9ababb55ea46bb937430419a97adc26c3c99ff22
Diegowned <dgt156423@gmail.com>
2023-05-09 14:16:19 -0400

    Particle Effects

--- Sphere test/Assets/Scripts/ImageFillController.cs
+++ Sphere test/Assets/Scripts/ImageFillController.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class ImageFillController : MonoBehaviour
 {
+    public float speedlineValue = 5f; // the value at which the particle effect should be activated
+    public ParticleSystem particleEffectPrefab; // the particle effect prefab to be activated
     public Image imageToControl;
     public int maxValue;
     public int currentValue;
@@ -12,11 +14,19 @@ public class ImageFillController : MonoBehaviour
 
 
     private void Start()
+
     {
+        particleEffectPrefab = GameObject.Find("Particle System").GetComponent<ParticleSystem>(); ;
         getVelocity = GetComponent<GetVelocity>();
     }
     private void Update()
     {
+        if (currentValue > speedlineValue)
+        {
+            particleEffectPrefab.Play();
+
+        }
+
         currentValue = getVelocity.speedForUI;
 
         float fillAmount = (float)currentValue / maxValue;
