225a8265776e5d6c225176afbc77d653ba2aa19d
Diegowned <dgt156423@gmail.com>
2023-06-27 14:52:51 -0400

    Audio Mixer and Player Prefs ready

--- Sphere test/Assets/Scripts/VolumeSettings.cs
+++ Sphere test/Assets/Scripts/VolumeSettings.cs
@@ -9,6 +9,8 @@ public class VolumeSettings : MonoBehaviour
 
     [SerializeField] private AudioMixer mainMixer;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider effectSlider;
+    [SerializeField] private Slider masterSlider;
 
     private void Start()
     {
@@ -18,10 +20,20 @@ public class VolumeSettings : MonoBehaviour
         }
         else
         {
+            SetMasterVolume();
             SetMusicVolume();
+            SetEffectVolume();
         }
 
         musicSlider = GameObject.Find("Canvas/Options Panel/Music Sound Slider").GetComponent<Slider>();
+    }
+
+
+    public void SetEffectVolume()
+    {
+        float volume = effectSlider.value;
+        mainMixer.SetFloat("mixerSoundEffects", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("effectVolume", volume);
     }
 
     public void SetMusicVolume()
@@ -31,12 +43,24 @@ public class VolumeSettings : MonoBehaviour
         PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
+    public void SetMasterVolume()
+    {
+        float volume = masterSlider.value;
+        mainMixer.SetFloat("mixerMaster", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("masterVolume", volume);
+    }
+
+
     private void LoadVolume()
 
     {
+        masterSlider.value = PlayerPrefs.GetFloat("masterVolume");
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        effectSlider.value = PlayerPrefs.GetFloat("effectVolume");
 
+        SetMasterVolume();
         SetMusicVolume();
+        SetEffectVolume();
     }
 
 }
