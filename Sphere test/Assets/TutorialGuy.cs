7783ef3645fa14c9be66da01a26bb75abaf0390d
Hairday <macabreravasquez@gmail.com>
2023-06-30 17:59:27 -0400

    Implemented conversation and animated UI sprite
    bird is the word

--- Sphere test/Assets/TutorialGuy.cs
+++ Sphere test/Assets/TutorialGuy.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DialogueEditor;
 
 public class TutorialGuy : MonoBehaviour
 {
     public AnimationCurve speed;
+    public NPCConversation Tutorial;
+    public GameObject soHead;
     void Update()
 
     {
         transform.position = new Vector3(transform.position.x, speed.Evaluate((Time.time % speed.length)), transform.position.z);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        soHead.SetActive(true);
+        ConversationManager.Instance.StartConversation(Tutorial);
+        
     }
 }
