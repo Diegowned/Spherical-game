7f1e15a08abce97e99fdc55e12888a31f79ecde1
Hairday <macabreravasquez@gmail.com>
2023-07-04 17:56:13 -0400

    General Fixes
    El dialogo del tutorial no puede ser activado 2 veces en el mismo intento, lvl 3 está escalado apropiadamente al jugador

--- Sphere test/Assets/TutorialGuy.cs
+++ Sphere test/Assets/TutorialGuy.cs
@@ -11,6 +11,8 @@ public class TutorialGuy : MonoBehaviour
     public GameObject player;
     public GameObject crosshair;
     public Timer timerScript;
+    private bool hasBeenActivated = false;
+    public GameObject self;
 
 
     private void Start()
@@ -25,17 +27,19 @@ public class TutorialGuy : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
                 ConversationManager.Instance.PressSelectedOption();
         }
-        if (ConversationManager.Instance.IsConversationActive == false)
+        if (ConversationManager.Instance.IsConversationActive == false && hasBeenActivated == true)
         {
             timerScript.isPaused = false;
             soHead.SetActive(false);
             player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
             crosshair.SetActive(true);
+            Destroy(self.GetComponent<BoxCollider>());
         }
         transform.position = new Vector3(transform.position.x, speed.Evaluate((Time.time % speed.length)), transform.position.z);
     }
     public void OnTriggerEnter(Collider other)
     {
+        hasBeenActivated = true;
         timerScript.isPaused = true;
         player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         crosshair.SetActive(false);
